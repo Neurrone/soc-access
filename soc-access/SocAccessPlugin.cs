a91f35b8838f9bfd967838feec05ebdaf28d9034
Dickson Tan <dickson.tan.2013@gmail.com>
2026-08-14 16:31:13 +0800

    bump to v1.0.0

--- soc-access/SocAccessPlugin.cs
+++ soc-access/SocAccessPlugin.cs
@@ -19,7 +19,7 @@ namespace SongsOfConquestAccess
     {
         public const string PluginGuid = "songs.of.conquest.access";
         public const string PluginName = "Songs of Conquest Access";
-        public const string PluginVersion = "0.7.4";
+        public const string PluginVersion = "1.0.0";
 
         internal static SocAccessPlugin Instance { get; private set; }
 
